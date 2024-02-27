7244e854befd726193aa24f8f68eede9195925d8
Elaidzha1940 <eli080293@gmail.com>
2024-02-27 20:05:52 +0300

    SplineOnBoarding

---
+++ SplineOnBoarding/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: SplineOnBoarding
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.02.2024
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+          
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplineOnBoarding/SplineOnBoardingApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: SplineOnBoarding
+//  File: SplineOnBoardingApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.02.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SplineOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
